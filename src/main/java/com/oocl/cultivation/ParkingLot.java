cc7d7468b83f3e44ff94476c41378cc517ca4691
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-24 15:55:57 +0800

    [ParkingLot][penny]feature: implement should_return_ticket_when_park_given_car test

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,9 +1,8 @@
 package com.oocl.cultivation;
 
-import sun.security.krb5.internal.Ticket;
 
 public class ParkingLot {
     public Ticket park(Car car) {
-        return null;
+        return new Ticket();
     }
 }
--- src/test/java/com/oocl/cultivation/test/ParkingLotFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotFacts.java
@@ -2,15 +2,26 @@ package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.Car;
 import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.Ticket;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import sun.security.krb5.internal.Ticket;
 
 import java.util.concurrent.locks.LockSupport;
 
 public class ParkingLotFacts {
     @Test
     void should_return_ticket_when_park_given_car() {
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        //when
+        Ticket carTicket = parkingLot.park(car);
+        //then
+        Assertions.assertNotNull(carTicket);
+    }
+
+    @Test
+    void should_feach_car_when_park_given_ticket() {
         //given
         Car car = new Car();
         ParkingLot parkingLot = new ParkingLot();
