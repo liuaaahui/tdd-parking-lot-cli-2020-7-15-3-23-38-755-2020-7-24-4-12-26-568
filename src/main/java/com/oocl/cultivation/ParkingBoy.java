3086f1efe11ede229067f5e3497ef9242aa578bf
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-26 16:39:22 +0800

    [story3][penny]feature: make Story 1 and Story 2 to be satisfied

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -12,10 +12,23 @@ public class ParkingBoy {
     public Ticket park(Car car) {
         int parkingLotNumber = 0;
         ParkingLot parkingLot = this.parkingLotList.get(parkingLotNumber);
-        if (parkingLot.getParkRooms().size() >= 10) {
+        if (parkingLot.getParkRooms().size() >= 10 && this.parkingLotList.size()>1) {
             parkingLotNumber++;
             parkingLot = this.parkingLotList.get(parkingLotNumber);
         }
         return parkingLot.park(car);
+    }
+
+    public Car feach(Ticket carTicket) {
+        if (carTicket == null) {
+            System.out.print("Please provide your parking ticket.\n");
+            return null;
+        }
+        Car car = null;
+        for (ParkingLot parkingLot : this.parkingLotList) {
+            car = parkingLot.feach(carTicket);
+            if (car != null) return car;
+        }
+        return car;
     }
 }
--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -5,12 +5,26 @@ import com.oocl.cultivation.ParkingBoy;
 import com.oocl.cultivation.ParkingLot;
 import com.oocl.cultivation.Ticket;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingBoyTest {
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void init() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
+
     @Test
     void should_park_anthor_parklot_when_park_given_11_cars() {
         //given
@@ -27,5 +41,137 @@ public class ParkingBoyTest {
         Ticket ticket = parkingBoy.park(new Car());
         //then
         Assertions.assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_ticket_when_park_given_car() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(firstParkingLot);
+        parkingLotList.add(secondParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        //when
+        Ticket ticket = parkingBoy.park(new Car());
+        //then
+        Assertions.assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_correspond_car_when_feach_given_ticket() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(firstParkingLot);
+        parkingLotList.add(secondParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        Car car = new Car();
+        Ticket ticket = parkingBoy.park(car);
+        //when
+        Car feachCar = parkingBoy.feach(ticket);
+        //then
+        Assertions.assertEquals(car,feachCar);
+    }
+
+    @Test
+    void should_return_correspond_cars_when_feach_given_lot_tickets() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(firstParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        Ticket firstTicket = parkingBoy.park(firstCar);
+        Ticket secondTicket = parkingBoy.park(secondCar);
+        //when
+        Car carWithFirstTicket = parkingBoy.feach(firstTicket);
+        Car carWithSecondTicket = parkingBoy.feach(secondTicket);
+        //then
+        Assertions.assertNotNull(carWithFirstTicket);
+        Assertions.assertNotNull(carWithSecondTicket);
+        Assertions.assertEquals(carWithFirstTicket,firstCar);
+        Assertions.assertEquals(carWithSecondTicket,secondCar);
+    }
+
+    @Test
+    void should_return_null_when_feach_given_no_ticket() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(firstParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        Car car = new Car();
+        parkingBoy.park(car);
+        //when
+        Car feachCar = parkingBoy.feach(null);
+        //then
+        Assertions.assertNull(feachCar);
+        Assertions.assertNotEquals(car,feachCar);
+    }
+
+    @Test
+    void should_return_null_when_feach_given_used_ticket() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(firstParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        Car car = new Car();
+        Ticket ticket = parkingBoy.park(car);
+        //when
+        parkingBoy.feach(ticket);
+        Car feachCar = parkingBoy.feach(ticket);
+        //then
+        Assertions.assertNull(feachCar);
+    }
+
+    @Test
+    void should_have_error_message_when_feach_given_used_ticket() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(firstParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        Car car = new Car();
+        Ticket ticket = parkingBoy.park(car);
+        //when
+        parkingBoy.feach(ticket);
+        parkingBoy.feach(ticket);
+        //then
+        Assertions.assertTrue(systemOut().endsWith("Unrecognized parking ticket.\n"));
+    }
+
+    @Test
+    void should_have_error_message_when_feach_given_no_ticket() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(firstParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        Car car = new Car();
+        parkingBoy.park(car);
+        //when
+       parkingBoy.feach(null);
+        //then
+        Assertions.assertTrue(systemOut().endsWith("Please provide your parking ticket.\n"));
+    }
+
+    @Test
+    void should_have_error_message_when_hava_one_parklot_given_11_cars() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(firstParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        for(int count = 0;count <10;count++){
+            parkingBoy.park(new Car());
+        }
+        //when
+        parkingBoy.park(new Car());
+        //then
+        Assertions.assertTrue(systemOut().endsWith("Not enough position.\n"));
     }
 }
