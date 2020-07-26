cd7bd27e53637bb252ba54cb4279478c4505c186
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-26 14:12:04 +0800

    [story2][penny]feature: add should_have_error_message_when_feach_given_used_ticket test

--- src/test/java/com/oocl/cultivation/test/ResponseMessageTest.java
+++ src/test/java/com/oocl/cultivation/test/ResponseMessageTest.java
@@ -2,6 +2,7 @@ package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.Car;
 import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.Ticket;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,6 +30,19 @@ public class ResponseMessageTest {
         parkingLot.park(car);
         //when
         Car feachCar = parkingLot.feach(null);
+        //then
+        Assertions.assertTrue(systemOut().endsWith("Unrecognized parking ticket.\n"));
+    }
+
+    @Test
+    void should_have_error_message_when_feach_given_used_ticket() {
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        Ticket ticket = parkingLot.park(car);
+        //when
+        parkingLot.feach(ticket);
+        parkingLot.feach(ticket);
         //then
         Assertions.assertTrue(systemOut().endsWith("Unrecognized parking ticket.\n"));
     }
