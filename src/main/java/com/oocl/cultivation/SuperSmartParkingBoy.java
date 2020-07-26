f01e402953d568938b44ac55d2f5a3d537cc2721
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-26 18:52:20 +0800

    [story5][penny]feature: implement should_park_in_the_arger_available_position_rate_parkingLot_when_super_smart_parkingBoy_park_given_car test

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -2,13 +2,19 @@ package com.oocl.cultivation;
 
 import java.util.List;
 
-public class SuperSmartParkingBoy extends ParkingBoy{
+public class SuperSmartParkingBoy extends ParkingBoy {
 
     public SuperSmartParkingBoy(List<ParkingLot> parkingLotList) {
         super(parkingLotList);
     }
 
     public Ticket park(Car car) {
-        return super.park(car);
+        ParkingLot parkingLotFlag = this.getParkingLotList().get(0);
+        for (ParkingLot parkingLot : this.getParkingLotList()) {
+            if (parkingLot.getParkRooms().size() * parkingLotFlag.getCapacity() < parkingLotFlag.getParkRooms().size() * parkingLot.getCapacity()) {
+                parkingLotFlag = parkingLot;
+            }
+        }
+        return parkingLotFlag.park(car);
     }
 }
